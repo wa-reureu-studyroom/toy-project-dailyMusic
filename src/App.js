1e5313323fa69fd966b276453c568121bb4a3bf4
yeon0226 <w01086248649@gmail.com>
2023-05-29 15:31:20 +0900

    날씨, 명언, 라이브러리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
+import PlayList from './component/PlayList.js';
+import Weather from './component/weather.js';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PlayList />
+      <Weather />
     </div>
-  );
+  )
 }
 
 export default App;
