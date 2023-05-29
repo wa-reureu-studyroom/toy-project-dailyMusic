51e6a3900e4aade5ca24376312f5619500f459b6
Emma-Hyejin <cjd191058@naver.com>
2023-05-29 23:04:46 +0900

    feat: 페이지 폴더 및 파일 생성

---
+++ src/pages/Community.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Community.css';
+
+const Community = () => {
+    return(
+        <>
+            <h1>Community</h1>
+        </>
+    )
+}
+
+export default Community;
---
+++ src/pages/MusicPage.jsx
@@ -0,0 +1,12 @@
+import React from'react';
+import './MusicPage.css';
+
+const MusicPage = () => {
+    return(
+        <>
+            <h1>Music Page</h1>
+        </>
+    )
+}
+
+export default MusicPage;
